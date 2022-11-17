624c0ae7596f8b0fdf2c9b31299849a4cad1bd86
ecriturebrailmaistupeuxrienvoir <90462132+ecriturebrailmaistupeuxrienvoir@users.noreply.github.com>
2022-11-17 16:42:23 +0100

    Création fonction create deck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Main
+import random
 
 class player :
   __init__ (self, name)  :
@@ -15,3 +16,17 @@ class Card :
     self.nbr_gem = nbr_gem
     self.relic = relic
     self.val_relic = val_relic
+    
+def create_deck() :
+  deck = []
+  val_treasure = [1, 2, 3, 4, 5, 5, 7, 7, 9, 11, 11, 13, 14, 15, 17]
+  val_relic = [5, 5, 5, 10, 10]
+  for i in range (15) :
+    deck.append(Card(False, 0, True, val_treasure[i], False, 0))
+  for i in range (1, 6) :
+    for j in range (3) :
+      deck.append(Card(True, i, False, 0, False, 0))
+  for i in range (5) :
+    deck.append(Card(False, 0, False, 0, True, val_relic[i]
+  deck.shuffle()
+  return deck
