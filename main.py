b0f2513f4d509140769a7507934db4e8f75cb6cd
ecriturebrailmaistupeuxrienvoir <90462132+ecriturebrailmaistupeuxrienvoir@users.noreply.github.com>
2022-11-22 16:26:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,27 +9,43 @@ class player :
         self.in_game = False
 
 class Card :
-    def __init__(self, danger, type_danger, treasure, nbr_gem, relic, val_relic) :
+    def __init__(self, danger, type_danger, treasure, nbr_gem, relic) :
         self.danger = danger
         self.type_danger = type_danger
         self.treasure = treasure
         self.nbr_gem = nbr_gem
         self.relic = relic
-        self.val_relic = val_relic
     
 def create_deck() :
     deck = []
     val_treasure = [1, 2, 3, 4, 5, 5, 7, 7, 9, 11, 11, 13, 14, 15, 17]
-    value_relic = [5, 5, 5, 10, 10]
     for i in range (15) :
         deck.append(Card(False, 0, True, val_treasure[i], False, 0))
     for i in range (1, 6) :
         for j in range (3) :
             deck.append(Card(True, i, False, 0, False, 0))
     for i in range (5) :
-        deck.append(Card(False, 0, False, 0, True, value_relic[i]))
+        deck.append(Card(False, 0, False, 0, True,))
     random.shuffle(deck)
     return deck
+
+def start_game():
+    #Récupération du nombre de joueurs
+    nb_player = int(input("Combien de joueurs êtes-vous ?"))
+    while nb_player > 8 or nb_player < 3 :
+        nb_player = int(input("Nombre de joueurs incorrects, il faut être entre 3 et 8 joueurs"))
+
+    #Création de la liste des joueurs
+    list_player = []
+    for i in range (nb_player) :
+        print("Joueur ",i+1,",entrez votre nom")
+        name = str(input())
+        list_player.append(name)
+    
+    #Création deck
+    deck=create_deck()
+    
+    return list_player, deck
 
 def draw(deck,deck_shown,list_player):
     '''
